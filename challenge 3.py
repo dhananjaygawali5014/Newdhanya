8096122431d38da9fa97cd0831d73b997f995d90
dhananjaygawali5014 <48133286+dhananjaygawali5014@users.noreply.github.com>
2020-05-21 10:36:13 +0530

    Add files via upload

---
+++ challenge 3.py
@@ -0,0 +1,10 @@
+for i in range (1,100):
+    if ((i%3==0)&(i%5==0)):
+        print ('Black Red')
+
+    elif (i%3==0):
+        print ('Black')
+    elif (i%5==0):
+        print ('Red')
+    else :
+        print (i)
